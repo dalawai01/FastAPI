3ee4cbdb9f1c264fefec5fd1828e7be37ff3ab65
dalawai01 basu <basudalawai27@gmail.com>
2024-10-02 06:32:39 +0000

    FastAPI

---
+++ FastAPI/tut3/app/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+class ItemBase(BaseModel):
+    title: str
+    description: str | None = None
+    price: float
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int    
